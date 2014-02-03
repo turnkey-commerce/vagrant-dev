aceee52a81508dcdc1ad09bebead5ebbdb6c278b
James Culbertson <info@turnkey-commerce.com>
2014-02-02 22:16:44 -0600

    Add installation of MongoDB.
    Add MongoDB, mongopy, and Python file for testing MongoDB.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,3 +12,10 @@ apt-get install -y vim.tiny
 # Python-related.
 apt-get install -y python-pip
 pip install virtualenv
+pip install pymongo
+
+# MongoDB install
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update
+apt-get install -y mongodb-10gen
